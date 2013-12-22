6571de1ae9aab962e2a6676b6c2c029945cd03b1
Bill J <bill@bills-air.home>
2013-12-22 17:46:40 -0500

    Add char chounter

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,18 +1,13 @@
 <?php if(isset($user)): ?>
 	<div id="wrapper2">
 		<div id="newsletter" class="container">
-			<div class="title">
-				<h2>Add a new post</h2>
-				<span class="byline">
-					Please type your message in the box blow.
-				</span> 
-			</div>
 			<div class="content">
 				<form method='post' action='/posts/p_add'>
 					<div class="row half">
 						<div class="12u">
-							<textarea name='content' placeholder="Message"></textarea>
+							<textarea name='content' placeholder="Message" onkeyup="countChar(this)"></textarea>
 							Maximum characters: 500<br>
+							<div id="charNum"> </div>
 						</div>
 					</div>
 					<div class="row">
