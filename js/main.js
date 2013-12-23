506878d35d18c046973589320bb0db95a0d8d552
Bill J <bill@bills-air.home>
2013-12-22 19:37:10 -0500

    New sound

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 function countChar(val){
     if (val.value.length >= 500)      
     	val.value = val.value.substring(0, 500);
-    $('#charNum').text(500 - val.value.length);    
+    $('#charNum').text(500 - val.value.length +' '+'left');    
 };
 
-function playKey() {
-	var audio = new Audio("sound/click.mp3");	
-	audio.play();
-};
-
-document.addEventListener('keydown', function(event) {
-	playKey();
-});
+function playAKey() {
+	var audio = document.getElementById("sound/click.mp3");
+	if (audio) audio.play();
+}
 		
+document.addEventListener('keydown', function(event) {
+	playAKey();
+});
